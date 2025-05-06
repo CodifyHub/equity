ba407788ee9a392cc48a34c90f9b2c6eb26dae62
OfficialOzioma <dikep15@gmail.com>
2025-05-06 13:40:04 +0100

    i added the build folder

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -7,7 +7,6 @@
     <section class="bg-gray-50 py-8 antialiased dark:bg-gray-900 md:py-12">
         <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
 
-
             <div class="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4">
                 <div class="rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
                     <div class="h-56 w-full">
